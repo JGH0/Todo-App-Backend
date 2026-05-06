3438888314f01f615ede824d58a61c4196f52054
Cametendo <cameronmathis08@gmail.com>
2026-05-06 14:24:53 +0200

    Fix merge conflict

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-<<<<<<< Updated upstream
-=======
 $routes->get('/themes', 'ThemeStore::index');
 $routes->post('/themes/upload', 'ThemeStore::upload');
 $routes->get('/themes/preview/(:segment)', 'ThemeStore::preview/$1');
->>>>>>> Stashed changes
