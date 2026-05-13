daa6ec8b1e8320153cab27c8a2ac77e5035b0a38
Cametendo <cameronmathis08@gmail.com>
2026-05-13 16:06:27 +0200

    Merge feature/marketplace into main

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/themes', 'ThemeStore::index');
+$routes->post('/themes/upload', 'ThemeStore::upload');
+$routes->get('/themes/preview/(:segment)', 'ThemeStore::preview/$1');
 
 // ============================================================================
 // API Routes - Version 1.0
